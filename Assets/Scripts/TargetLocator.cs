696725fd9b18bbd12399d5cf3ba0387202c9a110
Nataliia <natalia.egorova978@gmail.com>
2022-07-07 11:13:11 +0400

    Made ram target closest enemy (ugh)

--- Assets/Scripts/TargetLocator.cs
+++ Assets/Scripts/TargetLocator.cs
@@ -6,18 +6,53 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform wearpon;
-    [SerializeField] Transform target;
-    private void Start() 
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
+    [SerializeField] float towerRange = 1f;
+    [SerializeField] ParticleSystem projectileParticle;
+    Transform target;
+    
     private void Update() 
     {
+        FindClosestTarget();
         AimWearpon();
+    }
+
+    private void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach (Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+        target = closestTarget;
     }
 
     private void AimWearpon()
     {
+        float targetDistance = Vector3.Distance(transform.position, target.transform.position);
         wearpon.LookAt(target);
+
+        if(targetDistance < towerRange)
+        {
+            Attack(true);
+        }
+        else 
+        {
+            Attack(false);
+        }
+    }
+
+    private void Attack(bool isActive)
+    {
+        var emissionModule = projectileParticle.emission;
+        emissionModule.enabled = isActive;
     }
 }
