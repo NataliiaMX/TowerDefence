409b2b9aa326dd9a9c120c64b0b2672f1f3f2680
Nataliia <natalia.egorova978@gmail.com>
2022-07-12 12:36:48 +0400

    Made updatable UI gold counter

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -2,34 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] int startingBalance = 150;
-    [SerializeField] int currentBalance;
-    public int CurrentBalance { get {return currentBalance;} }
     
-    private void Awake() 
+    [SerializeField] int currentBalance;
+    public int CurrentBalance { get { return currentBalance; } }
+
+    [SerializeField] TextMeshProUGUI displayBalance;
+
+    void Awake()
     {
         currentBalance = startingBalance;
-    }
-    public void Deposit (int amount)
-    {
-        currentBalance += Mathf.Abs(amount); //abs to remove possible negative sign of amount
+        UpdateDisplay();
     }
 
-    public void Withdraw (int amount)
+    public void Deposit(int amount)
+    {
+        currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
+    }
+
+    public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
-        if (currentBalance <= 0)
+        if(currentBalance < 0)
         {
-            Debug.Log("lost");
+            //Lose the game;
             ReloadScene();
         }
     }
 
-    private void ReloadScene ()
+    void UpdateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
+    }
+
+    void ReloadScene()
     {
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.buildIndex);
